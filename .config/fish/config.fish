14877e17a11a365bc6e814e8869558610a6d268d
Stephen Reynolds <mail@stephenreynolds.dev>
2022-10-24 19:55:00 -0400

    Customize fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,14 +5,7 @@ end
 function fish_greeting
 end
 
+alias vim="nvim"
 alias dotfiles="git --git-dir $HOME/.dotfiles --work-tree=$HOME"
 
-set -g theme_powerline_fonts yes
-set -g theme_nerd_fonts yes
-set -g theme_display_git_stashed_verbose yes
-set -g theme_display_git_master_branch yes
-set -g theme_display_git_untracked yes
-set -g theme_display_git_dirty yes
-set -g theme_display_nvm yes
-set -g theme_display_virtualenv yes
-set -g theme_color_scheme zenburn
+set -g theme_display_date no
