4dfa45025620f351a562a57bee0a535726c025a5
Stephen Reynolds <mail@stephenreynolds.dev>
2022-12-30 06:41:20 -0500

    Remove client borders

--- .config/awesome/themes/kamicha/theme.lua
+++ .config/awesome/themes/kamicha/theme.lua
@@ -30,7 +30,7 @@ theme.bg_systray = theme.bg_normal
 
 -- {{{ Borders
 theme.useless_gap   = dpi(5)
-theme.border_width  = dpi(2)
+theme.border_width  = dpi(0)
 theme.border_normal = "#3F3F3F"
 theme.border_focus  = "#6F6F6F"
 theme.border_marked = "#CC9393"
