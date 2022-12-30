e5ad1d0d96aa0e8ccebbb23ee3e63cc27162396a
Stephen Reynolds <mail@stephenreynolds.dev>
2022-12-30 06:43:37 -0500

    Add startup processes

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -535,3 +535,24 @@ client.connect_signal("request::titlebars", function(c)
         layout = wibox.layout.align.horizontal
     }
 end)
+
+local function execute_once(command)
+    local command_program_name = command
+    local first_space_position = command:find(" ")
+    if first_space_position ~= nil then
+        command_program_name = command:sub(0, first_space_position - 1)
+    end
+    awful.util.spawn_with_shell("pgrep -u $USER -x " .. command_program_name .. " > /dev/null || (" .. command .. ")")
+end
+
+local function execute_commands(commands)
+    for _, command in ipairs(commands) do
+        execute_once(command)
+    end
+end
+
+execute_commands({
+    "picom --config $HOME/.config/picom/picom.conf",
+    "nm-applet",
+    "blueman-applet"
+})
