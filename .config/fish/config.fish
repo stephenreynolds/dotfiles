72956f78e1672b52cfbaad14c10522e488730b37
Stephen Reynolds <mail@stephenreynolds.dev>
2023-06-11 23:10:00 -0400

    Remove fzf background

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,6 @@ fish_add_path $HOME/.config/scripts $HOME/.local/bin $HOME/bin /usr/local/bin $H
 
 # FZF (fuzzy finder)
 set -gx FZF_DEFAULT_OPTS "--extended \
-    --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
     --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
     --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 set -gx FZF_DEFAULT_COMMAND "ag --ignore .git -g ''"
