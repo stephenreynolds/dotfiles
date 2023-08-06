466b475b5d6af3d880f35a708e52fdfc36591b58
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-06 13:51:56 -0400

    Add fzf

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -14,7 +14,6 @@
     tree-sitter
     nodejs
     rustup
-    fzf
     silver-searcher
     fd
     unzip
@@ -196,6 +195,29 @@
     };
   };
 
+  programs.fzf = {
+    enable = true;
+    enableBashIntegration = true;
+    enableFishIntegration = true;
+    # Catppuccin mocha theme
+    colors = {
+        "bg+" = "#313244";
+        bg = "#1e1e2e";
+        spinner = "#f5e0dc";
+        hl = "#f38ba8";
+        fg = "#cdd6f4";
+        header = "#f38ba8";
+        info = "#cba6f7";
+        pointer = "#f5e0dc";
+        marker = "#f5e0dc";
+        "fg+" = "#cdd6f4";
+        prompt = "#cba6f7";
+        "hl+" = "#f38ba8";
+    };
+    defaultCommand = "ag --ignore .git -g ''";
+    defaultOptions = [ "--extended" ];
+    fileWidgetCommand = "ag --hidden --ignore .git -g '' --ignore '.cache' --ignore '.dotfiles' --ignore '.local' --ignore '.mozilla'";
+  };
 
   programs.go = {
     enable = true;
