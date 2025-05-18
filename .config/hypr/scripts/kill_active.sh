f96d93b2d77530928879f9f993ebb2ca489363bf
Stephen Reynolds <mail@stephenreynolds.dev>
2025-05-18 16:29:57 -0400

    hyprland: switch to previous workspace on closing last window on workspace

--- .config/hypr/scripts/kill_active.sh
+++ .config/hypr/scripts/kill_active.sh
@@ -1,6 +1,22 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
-case $(hyprctl activewindow -j | ${jaq} -r ".class") in
+# Get all the information we need in a single hyprctl call
+active_window=$(hyprctl activewindow -j)
+active_class=$(echo "$active_window" | jaq -r ".class")
+workspace_id=$(echo "$active_window" | jaq -r '.workspace.id')
+
+# Get all clients and check if this is the last window on the workspace in one operation
+clients_json=$(hyprctl clients -j)
+window_count=$(echo "$clients_json" | jaq -r "[.[] | select(.workspace.id == $workspace_id)] | length")
+
+# Determine if this is the last window
+last_window=false
+if [ "$window_count" -eq 1 ]; then
+    last_window=true
+fi
+
+# Handle window closing based on class
+case "$active_class" in
 "discord")
     xdotool getactivewindow windowunmap
     hyprctl dispatch killactive ""
@@ -9,3 +25,8 @@ case $(hyprctl activewindow -j | ${jaq} -r ".class") in
     hyprctl dispatch killactive
     ;;
 esac
+
+# If this was the last window on the workspace, switch to the previous workspace
+if [ "$last_window" = true ] && [ "$workspace_id" -ne -99 ]; then
+    hyprctl dispatch workspace m-1
+fi
