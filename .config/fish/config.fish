5a21f40d1ee59179f738e76b0e8e9264095d3ee7
Stephen Reynolds <mail@stephenreynolds.dev>
2025-07-10 21:14:36 -0400

    wsl: add wsl ssh agent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -82,6 +82,9 @@ set --export PATH $BUN_INSTALL/bin $PATH
 # pnpm
 set -gx PNPM_HOME "/home/stephen/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# wsl2 ssh agent
+eval "$(/usr/sbin/wsl2-ssh-agent)"
